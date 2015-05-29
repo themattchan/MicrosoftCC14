0a91f5d7f3e114c94675c3c2091e6ce51b413dd2
Matthew Chan <matt@themattchan.com>
2015-05-29 02:02:23 -0700

    edits

--- NDrome/ndrome.hs
+++ NDrome/ndrome.hs
@@ -1,16 +1,21 @@
 import Data.List.Split
-splitDrome :: String -> (String, Int)
-splitDrome line = let sp = splitOn "|" line in
-                  (head sp, read $ head $ tail sp)
 
-dromeTest (str, n) = let chunked = chunksOf n str in
-                     case (chunked == reverse chunked) of
-                       True  -> "|1"
-                       False -> "|0"
 
-removeCRLF line = case splitDrome line of
-                    (x,y) -> x++"|"++(show y)
+splitDrome :: String -> (String, String)
+splitDrome line = let (x:y:_) = splitOn "|" line in
+                  (x, y)
 
+dromeTest :: (String, String) -> String
+dromeTest (str, n) = let chunked = chunksOf (read n) str in
+                     if chunked == reverse chunked
+                     then "|1"
+                     else "|0"
+
+removeCRLF :: String -> String
+removeCRLF line = let (x,y) = splitDrome line in
+                    x ++ "|" ++ y
+
+main :: IO ()
 main = do
   f <- readFile "ActualInput.txt"
   let dromes = lines f
