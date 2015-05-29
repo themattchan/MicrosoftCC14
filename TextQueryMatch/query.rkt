240e9e4fb27479200e5b4abb67c3bd0c3b041025
Matthew Chan <matt@themattchan.com>
2015-05-29 16:49:41 -0700

    boilerplate done

--- TextQueryMatch/query.rkt
+++ TextQueryMatch/query.rkt
@@ -1,11 +1,12 @@
 #lang racket
 
 (define INFILE (file->lines "SampleInput.txt"))
+(define OUTFILE "SampleOutput.txt")
 
-(define num-of-pairs (string->number (car INFILE)))
+(define num-of-pairs (car INFILE))
 (define query-strings (cdr INFILE))
 
-(define (match? query string)
+(define (runQuery query string)
   "fuck"
   )
 
@@ -13,8 +14,11 @@
   [((? null?)) '()]
   [(`(,query ,string ,rest ...))
    ; => 
-   (cons (match? query string) (doList rest))])
+   (cons (runQuery query string) (doList rest))])
 
-(doList query-strings)
 
-;(map match? input-pairs)
+;(let ((OUTPUT (string-join 
+;            (cons num-of-pairs (doList query-strings))
+;            "\n")))
+;  (display-to-file OUTPUT OUTFILE
+;                   #:exists 'replace))
