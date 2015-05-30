1c2dab0888c8a10d79b75d596a20cd7a9364c91a
Matthew Chan <matt@themattchan.com>
2015-05-29 18:25:30 -0700

    sample out wrong on #6

--- TextQueryMatch/query.rkt
+++ TextQueryMatch/query.rkt
@@ -7,29 +7,22 @@
   (string-join (string-split str) ""))
 
 (define (runQuery query string)
-  (cond
-    ((< 0 (count-substring query string))
-     "true")
-    ((< 0 (count-substring query (unspace string))) 
-        "true")
-    (else "false")))
+  (if (or (< 0 (count-substring query string))
+          (< 0 (count-substring query (unspace string))))
+      "true" "false"))
 
 (define/match (doList lst)
   [((? null?)) '()]
   [(`(,query ,string ,rest ...))
-   ; =>
    (cons (runQuery query string) (doList rest))])
 
 ;; run
 (match-let* ([INFILE  "SampleInput.txt"]
              [OUTFILE "SampleOutput.txt"]
              [`(,num ,xs ...)
-              ; =>
               (map string-downcase (file->lines INFILE))]
              [OUTPUT
-              ; =>
-              (string-join (cons num (doList xs))
-                           "\n")])
+              (string-join (cons num (doList xs)) "\n")])
   
-    (display-to-file OUTPUT OUTFILE
+  (display-to-file OUTPUT OUTFILE
                    #:exists 'replace))
