99ca84a5dd444505ccff05cd361ec79321e3e71f
Matthew Chan <matt@themattchan.com>
2015-05-29 18:23:05 -0700

    sample out wrong on #6

--- TextQueryMatch/query.rkt
+++ TextQueryMatch/query.rkt
@@ -1,26 +1,35 @@
 #lang racket
 
+(define count-substring
+  (compose length regexp-match*))
+
+(define (unspace str)
+  (string-join (string-split str) ""))
+
 (define (runQuery query string)
-  "fuck"
-  )
+  (cond
+    ((< 0 (count-substring query string))
+     "true")
+    ((< 0 (count-substring query (unspace string))) 
+        "true")
+    (else "false")))
 
 (define/match (doList lst)
   [((? null?)) '()]
   [(`(,query ,string ,rest ...))
-   ; => 
+   ; =>
    (cons (runQuery query string) (doList rest))])
 
 ;; run
 (match-let* ([INFILE  "SampleInput.txt"]
              [OUTFILE "SampleOutput.txt"]
-             ; ----
-             [`(,num-of-pairs ,query-strings ...)
+             [`(,num ,xs ...)
               ; =>
-              (file->lines INFILE)]
-             [OUTPUT 
+              (map string-downcase (file->lines INFILE))]
+             [OUTPUT
               ; =>
-              (string-join 
-               (cons num-of-pairs (doList query-strings))
-               "\n")])
-  (display-to-file OUTPUT OUTFILE
+              (string-join (cons num (doList xs))
+                           "\n")])
+  
+    (display-to-file OUTPUT OUTFILE
                    #:exists 'replace))
